ed4d66af6011c62a186c13d94df68575aa7a5177
Pranita_0506 <you@example.com>
2025-07-14 10:35:04 +0530

    assingment

---
+++ ass17.js
@@ -0,0 +1,27 @@
+//var keyword
+
+var firstName = "Pranita";
+
+firstName ="Jahnavi";
+
+var firstName ="Minal"; 
+
+console.log(firstName);
+
+// let keyword
+
+let first_Name="Pranita";
+
+first_Name="Jahnavi";
+
+console.log(first_Name);
+
+//declare constant
+
+const pi = 3.14;
+console.log(pi);
+
+const pi1 = 3.14;
+pi1 = 3.15; 
+
+console.log(pi1);
